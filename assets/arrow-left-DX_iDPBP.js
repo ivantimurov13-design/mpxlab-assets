87c532d54e70009803d9d574ff7ba51b0df79f24
ivantimurov13-design <ivantimurov13@gmail.com>
2026-07-03 17:52:47 +0300

    deploy: редизайн дашборда (герой ЧП + KPI-полоса + тренд/донат + водопад/сигналы + склад) + дровер РНП (index-B-Vsm_-y)

--- assets/arrow-left-DX_iDPBP.js
+++ assets/arrow-left-DX_iDPBP.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DhaCLw1p.js";/**
+import{x as e}from"./index-B-Vsm_-y.js";/**
  * @license lucide-react v0.417.0 - ISC
  *
  * This source code is licensed under the ISC license.
